f180e5b943d0680d7ee01479c49b950c3053c040
checktheroads <matt@48ix.net>
2020-09-14 01:52:00 -0700

    add default runner for policy creation

--- routingpolicy/main.py
+++ routingpolicy/main.py
@@ -1,5 +1,8 @@
 """48 IX Particpant Route Server Config Management."""
 
+# Standard Library
+import asyncio
+
 # Project
 from routingpolicy.combine import generate_combined
 from routingpolicy.generate import generate_all
@@ -9,3 +12,7 @@ async def policy() -> None:
     """Generate participant policies and config files."""
     await generate_all()
     await generate_combined()
+
+
+if __name__ == "__main__":
+    asyncio.run(policy())
