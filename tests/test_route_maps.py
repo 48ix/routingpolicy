27b3c6d392536f6f6f58750c7f191a790c900929
checktheroads <matt@48ix.net>
2020-09-13 22:34:04 -0700

    fix invalid arguments

--- tests/test_route_maps.py
+++ tests/test_route_maps.py
@@ -7,6 +7,4 @@ from routingpolicy.config import params
 from routingpolicy.generate import route_map
 
 if __name__ == "__main__":
-    asyncio.run(
-        route_map(participant=params.participants[0], rs_id=1, loc_id=1, metro_id=1)
-    )
+    asyncio.run(route_map(participant=params.participants[0]))
