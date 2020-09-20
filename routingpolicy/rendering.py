2fbc426ac89001dd38827363090d2bbdefd79eb5
checktheroads <matt@48ix.net>
2020-09-20 16:12:43 -0700

    fix policies directory

--- routingpolicy/rendering.py
+++ routingpolicy/rendering.py
@@ -6,11 +6,14 @@ from pathlib import Path
 # Third Party
 from jinja2 import Environment, PackageLoader
 
+# Project
+from routingpolicy import APP_DIR
+
 template_env = Environment(
     loader=PackageLoader("routingpolicy", "templates"),
     enable_async=True,
     trim_blocks=True,
 )
 
-POLICIES_DIR = Path("/etc/48ix-routing-policy/policies")
+POLICIES_DIR = APP_DIR / "policies"
 GLOBALS_DIR = Path(__file__).parent / "global_policy"
