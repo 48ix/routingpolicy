e2ff0527f58a0b2d96345ce8c5ec148988422eb6
checktheroads <matt@48ix.net>
2020-09-20 13:31:58 -0700

    add secret generation function

---
+++ routingpolicy/generate_secret.py
@@ -0,0 +1,35 @@
+"""Convenience function for generating a fernet secret.
+
+Secret is used to encrypt & decrypt data between the Route Policy Server
+and the Route Server Agent.
+"""
+from cryptography.fernet import Fernet
+
+if __name__ == "__main__":
+    key = Fernet.generate_key().decode()
+    sep = "-" * (63 + len(key))
+    print(
+        """
+{sep}
+48 IX Inc.
+
+{sep}
+
+Route Policy Server to Route Server Agent Encryption Key: {k}
+
+{sep}
+On the Server, add this to /etc/48ix-routingpolicy/config.yaml:
+
+agent:
+  key: '{k}'
+
+{sep}
+On the Agent, add this to .bashrc:
+
+export ix_rsagent_secret='{k}'
+
+{sep}
+    """.format(
+            k=key, sep=sep
+        )
+    )
